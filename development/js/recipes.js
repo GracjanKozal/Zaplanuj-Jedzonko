0a07e4f29d940f88d8d44f1dfb82080e8354f3fa
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-09-08 00:00:20 +0200

    bug-fix

--- development/js/recipes.js
+++ development/js/recipes.js
@@ -148,8 +148,10 @@ document.addEventListener('DOMContentLoaded', function(){
             newLi.querySelector('.fa-edit').addEventListener('click', function () {
                 document.getElementById('new-recipe').classList.remove('not-visible');
                 document.getElementById('all-recipes').classList.add('not-visible');
-                document.getElementById('recipe-name-descr').innerText = newLi.children[1].innerText;
-                document.getElementById('ingredients-description').innerText = newLi.children[2].innerText;
+                var recipeName = document.getElementById('recipe-name-descr');
+                var recipeDescription = document.getElementById('ingredients-description');
+                recipeName.innerText = newLi.children[1].innerText;
+                recipeDescription.innerText = newLi.children[2].innerText;
                 document.getElementById('instrucion-list').innerHTML = newLi.children[4].innerHTML;
                 document.getElementById('ingredients-list').innerHTML = newLi.children[5].innerHTML;
             });
